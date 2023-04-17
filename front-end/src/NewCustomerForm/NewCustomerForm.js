dac8c45ffa1035736d48e43dd6397ac0b836ba45
josh <josh>
2023-04-17 00:56:49 -0700

    added Hello world new customer form

---
+++ front-end/src/NewCustomerForm/NewCustomerForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+class NewCustomerForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {      name: 'Please write an essay about your favorite DOM element.'    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    this.setState({name: event.target.value});  }
+  handleSubmit(event) {
+    alert('Name was submitted: ' + this.state.name);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input value={this.state.name} onChange={this.handleChange} />        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default NewCustomerForm
